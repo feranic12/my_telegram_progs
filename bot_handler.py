d04a9ebf3cc7f1729f5cb5e7c0b51b9d524b17e7
Feranic <feranic@yandex.ru>
2024-05-29 12:38:03 +0300

    Done bot without apiai

--- bot_handler.py
+++ bot_handler.py
@@ -1,4 +1,4 @@
-import requests, apiai, json, datetime
+import requests, json, datetime
 
 
 class BotHandler:
@@ -6,7 +6,6 @@ class BotHandler:
     def __init__(self, token):
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
-        self.api_ai_instance = apiai.ApiAI('82b7754ffbb14e0885eec615c190fc12', 'WordsFinderAIBot')
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
@@ -16,23 +15,14 @@ class BotHandler:
         return result_json
 
     def send_message(self, chat_id):
-        params = {'chat_id': chat_id, 'text': self.get_ai_response()}
+        if (self.get_request(self.get_last_update()) == "Пока!"):
+            response = "Пока," + self.get_name(self.get_last_update())
+        else:
+            response = "Привет," + self.get_name(self.get_last_update())
+        params = {'chat_id': chat_id, 'text': response}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
         return resp
-
-    def get_ai_response(self):
-        request = self.api_ai_instance.text_request()
-        request.lang = 'ru'
-        request.query = self.get_request(self.get_last_update())
-        if request.query == "Пока!":
-            return "Пока, " + self.get_name(self.get_last_update())
-        responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-        response = responseJson['result']['fulfillment']['speech']
-        if response:
-            return response
-        else:
-            return "Сударь, выражайтесь яснее, пожалуйста!"
 
     def get_last_update(self):
         results = self.get_updates()
